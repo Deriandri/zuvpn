8666b06b41df6f117c7a5758cdb8082dea24268a
Deriandri <deriandriananda82@gmail.com>
2026-02-23 19:58:09 -0800

    Update download links for zivpn service

--- zi2.sh
+++ zi2.sh
@@ -6,10 +6,10 @@ echo -e "Updating server"
 sudo apt-get update && apt-get upgrade -y
 systemctl stop zivpn.service 1> /dev/null 2> /dev/null
 echo -e "Downloading UDP Service"
-wget https://github.com/sweaterpink1999/udp-zivpn-sweaterpink/releases/download/udp-zivpn-sweaterpink_1.4.9/udp-zivpn-sweaterpink-linux-arm64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
+wget https://github.com/Deriandri/zuvpn/releases/download/udp-zivpn-sweaterpink_1.4.9/udp-zivpn-sweaterpink-linux-arm64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
 chmod +x /usr/local/bin/zivpn
 mkdir /etc/zivpn 1> /dev/null 2> /dev/null
-wget https://raw.githubusercontent.com/sweaterpink1999/udp-zivpn-sweaterpink/main/config.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
+wget https://raw.githubusercontent.com/Deriandri/zuvpn/main/config.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
 
 echo "Generating cert files:"
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=California/L=Los Angeles/O=Example Corp/OU=IT Department/CN=zivpn" -keyout "/etc/zivpn/zivpn.key" -out "/etc/zivpn/zivpn.crt"
